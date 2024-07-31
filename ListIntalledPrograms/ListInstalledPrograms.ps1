74bd048d17aefb7ea5760a886f2d61b9be6ade11
Alhena1558 <150757253+Alhena1558@users.noreply.github.com>
2024-07-31 14:05:45 +0100

    Delete ListIntalledPrograms directory

--- ListIntalledPrograms/ListInstalledPrograms.ps1
+++
@@ -1,9 +0,0 @@
-# Function to list installed programs
-function Get-InstalledPrograms {
-    $installedPrograms = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
-    $installedPrograms += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
-    $installedPrograms | Where-Object { $_.DisplayName } | Sort-Object DisplayName
-}
-
-# Run the function
-Get-InstalledPrograms | Format-Table -AutoSize
